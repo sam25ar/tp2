be98b7123aea1dce4f90a99dcc8d95afcdb28265
sam25ar <93477650+sam25ar@users.noreply.github.com>
2021-12-05 21:32:31 +0100

    Add files via upload

---
+++ LuckyController.php
@@ -0,0 +1,33 @@
+<?php
+// src/Controller/LuckyController.php
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController 
+{
+    public function number(): Response
+    {
+        $number = random_int(0, 100);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+
+       /**
+   * @Route("/lucky/number/{max}")
+    */
+    public function number1(int $max )
+    {
+        $number = random_int(0, $max);
+
+        return new Response(
+            '<html><body>Lucky number: '.$number.'</body></html>'
+        );
+    }
+    
+
+
+}
